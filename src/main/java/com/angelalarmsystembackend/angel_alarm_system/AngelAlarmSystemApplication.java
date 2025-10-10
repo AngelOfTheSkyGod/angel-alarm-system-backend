c6bee60db30b527ae622a40b946eda98c8e1bf54
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2025-10-10 17:01:12 -0500

    start

---
+++ src/main/java/com/angelalarmsystembackend/angel_alarm_system/AngelAlarmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.angelalarmsystembackend.angel_alarm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelAlarmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelAlarmSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelalarmsystembackend/angel_alarm_system/AngelAlarmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelalarmsystembackend.angel_alarm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelAlarmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
