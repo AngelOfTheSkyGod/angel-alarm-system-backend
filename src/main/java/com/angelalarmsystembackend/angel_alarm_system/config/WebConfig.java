3f62d80a4906b3310c2202771b30154faf9057e1
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2025-10-11 23:44:15 -0500

    added framework for network calls and application types

--- src/main/java/com/angelalarmsystembackend/angel_alarm_system/config/WebConfig.java
+++ src/main/java/com/angelalarmsystembackend/angel_alarm_system/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
-                .allowedOrigins("http://quinonesangel.com:5173")
+                .allowedOrigins("http://www.quinonesangel.com:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
